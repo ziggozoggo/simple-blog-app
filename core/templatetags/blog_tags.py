3a9ee08864934e1da2f085796d0aec32746b78bc
Max Tomilov <ziggozoggo@gmail.com>
2023-04-18 08:16:55 +0700

    add custom template tags

---
+++ core/templatetags/blog_tags.py
@@ -0,0 +1,28 @@
+from django import template
+from django.db.models import Count
+from core.models import Post
+
+register = template.Library()
+
+# simple_tag - возвращает строку
+# inclusion_tag - возвращает шаблон
+@register.simple_tag
+def total_posts():
+    """Количество опубликованных постов
+    """
+    return Post.published.count()
+
+@register.inclusion_tag('core/post/latest_posts.html')
+def show_latest_posts(count=5):
+    """Список последних опубликованных постов
+    """
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    """Список постов с наибольшим числом комментариев
+    """
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
